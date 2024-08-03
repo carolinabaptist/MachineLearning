f44333a1f2624b60b3b3b6d37d2f2bfa3e3d6577
Ana Carolina Baptista <119694053+carolinabaptist@users.noreply.github.com>
2024-08-03 19:43:06 +0100

    Create hw-1.py

---
+++ hw-1.py
@@ -0,0 +1,15 @@
+#Programming and critical analysis
+
+#Considering the breast.w.arff dataset available
+
+#Draw the class-conditional distributions per variable. Suggestion: use 3x3 plot grid.
+
+#Using a 10-fold cross validation with seed=<group number>, assess the accuracy of 𝑘NN
+under 𝑘 ∈ {3,5,7}, Euclidean distance and uniform weights to identify which 𝑘 is, empirically,
+less susceptible to the overfitting risk.
+
+#Fixing 𝑘 = 3, and assuming accuracy estimates are normally distributed, test the hypothesis
+#“𝑘NN is statistically superior to Naïve Bayes (multinomial assumption)”.
+
+#Given the empirical data collected along 5-7, enumerate two reasons that can underlie the
+#differences in performance between 𝑘NN and Naïve Bayes.
